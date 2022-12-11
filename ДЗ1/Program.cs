b818b49cf55e03d87e70110f8822fa1db250870f
Lera <valeriya-kenchadze@mail.ru>
2022-12-11 23:08:13 +0300

    Решила 3 задачи

---
+++ ДЗ1/Program.cs
@@ -0,0 +1,63 @@
+﻿// 1. Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+//     Console.WriteLine($"Число {a} больше числа {b}");
+
+// else
+//     Console.WriteLine($"Число {b} больше числа {a}");    
+
+// 2. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите первое число: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число: ");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// if (a > b)
+// {
+//     if (a > c)
+//         Console.WriteLine($"Число {a} самое большое");
+// }
+// else if (b > c)
+// {
+//      Console.WriteLine($"Число {b} самое большое");
+// }
+// else if (c > b)
+// {
+//     Console.WriteLine($"Число {c} самое большое");
+// }
+
+
+// 3. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// if (n % 2 == 0)
+// {
+//     Console.WriteLine($"Число {n} - чётное");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {n} - нечётное");
+// }
+
+// 4.Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int t = (0, num)
+
+while (t <= num)
+{
+  t +=1;
+  Console.WriteLine();
+  Console.Write(t + " ");
+}
